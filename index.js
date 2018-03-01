56ecfe5b36092f5be66f8e2f4f9eae892ae44b12
Rowan Collins <rowan.collins@netsells.co.uk>
2018-03-01 15:59:10 +0000

    glob working with webpack

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const glob = require('glob');
-const globRules = glob.sync('./rules/**/*.js');
+const globRules = glob.sync(__dirname + '/rules/**/*.js');
 
 module.exports = {
     //extends the all js files in the `./rules` directory
